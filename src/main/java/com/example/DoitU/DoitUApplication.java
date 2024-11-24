65b31a02c4c492ea6b9eb3b7e44c869e989e058c
player7571 <pxy7571@gmail.com>
2024-11-25 03:07:04 +0900

    Initial commit

---
+++ src/main/java/com/example/DoitU/DoitUApplication.java
@@ -0,0 +1,13 @@
+package com.example.DoitU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoitUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoitUApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DoitU/DoitUApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DoitU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoitUApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
