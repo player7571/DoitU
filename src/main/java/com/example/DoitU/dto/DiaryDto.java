5668352159008101cd2b50bd47a62afd95fd813f
player7571 <pxy7571@gmail.com>
2024-11-27 21:30:01 +0900

    feat: Calender 일기, 이모지 작성 기능 추가

---
+++ src/main/java/com/example/DoitU/dto/DiaryDto.java
@@ -0,0 +1,13 @@
+package com.example.DoitU.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DiaryDto {
+
+    private String diary;
+    private String date;
+
+}
---
+++ src/main/java/com/example/DoitU/dto/EmojiDto.java
@@ -0,0 +1,13 @@
+package com.example.DoitU.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmojiDto {
+
+    private String emoji;
+    private String date;
+
+}
